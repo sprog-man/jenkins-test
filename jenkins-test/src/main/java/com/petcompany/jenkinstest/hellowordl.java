103e861171ee38afa3b1bcbfac5ec7f7ce62a23f
sprog-man <2484511957@qq.com>
2026-04-13 19:46:27 +0800

    一个输出hello的springboot

---
+++ jenkins-test/src/main/java/com/petcompany/jenkinstest/hellowordl.java
@@ -0,0 +1,13 @@
+package com.petcompany.jenkinstest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class hellowordl {
+    @RequestMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
